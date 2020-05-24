0d4497f004cd0c44e1cdd064de8bc746ce07bf34
ilya <beqasmile@gmail.com>
2020-05-24 19:28:01 +0300

    add encoding decoding

---
+++ AsciiTable/AsciiTable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char charA = 'a';
+
+            int myX = 4;
+            int MYx = 6;
+
+            int x = 88;
+            Console.WriteLine(Convert.ToChar(x));
+
+            //Console.WriteLine(System.Convert.ToInt32(charA));
+            //Console.WriteLine(System.Convert.ToInt32('A'));
+
+            string word = "Hello Word";
+            Console.WriteLine(word);
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            foreach( char c in word) // to decimal
+            {
+                Console.Write(System.Convert.ToInt32(c) + "  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+            foreach (char c in word) // to hexadecimal
+            {
+                Console.Write(System.Convert.ToInt32(c).ToString("X") +  "  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+
+            for (int i=65; i<=90; i++)
+            {
+                Console.WriteLine(" " + i + " | " + System.Convert.ToChar(i) + " | |" + i.ToString("X"));
+            }
+
+            Console.ReadKey();
+        }
+
+      
+    }
+}
---
+++ AsciiTable/EncodingDecoding/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingDecoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please input word");
+
+            // encoding process
+            //------------------------------------------------------
+            String inputWord = Console.ReadLine();
+            List<Int32> encodedWord = Encoding(inputWord);
+            Queue<Int32> ourQueue = new Queue<Int32>();
+
+            foreach (Int32 encodedNum in encodedWord)
+            {
+                ourQueue.Enqueue(encodedNum);
+            }
+            //---------------------------------------------------------
+
+            List<int> queueList = new List<int>();
+            while (ourQueue.Count!=0)
+            {
+                queueList.Add(ourQueue.Dequeue());
+            }
+
+            String result = Decoding(queueList);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        public static List<Int32> Encoding(string myLongMessage)
+        {
+            List<Int32> result = new List<int>();
+
+            foreach (char c in myLongMessage)
+            {
+                result.Add(Convert.ToInt32(c) +3);
+            }
+            
+            return result;
+        }
+
+        public static string Decoding(List<int> myDecodedMessage)
+        {
+            String result = "";
+
+            foreach (int decodedNum in myDecodedMessage)
+            {
+                result = result + System.Convert.ToChar(decodedNum-3);
+            }
+
+
+            return result;
+        }
+    }
+}
