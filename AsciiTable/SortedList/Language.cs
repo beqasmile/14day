952b89b92b4b56ce6ee00ea3d8b95cc73b637d55
ilya <beqasmile@gmail.com>
2020-05-27 18:08:35 +0300

    language example

---
+++ AsciiTable/SortedList/Language.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    public class Language
+    {
+        private string name;
+        private int code;
+        private char[] lettersInLanguage;
+
+        public Language(int code, string name)
+        {
+            this.code = code;
+            this.name = name;
+
+
+            char firstLetter = 'A';
+
+            lettersInLanguage = new char[22];
+
+            for (int i=0; i<lettersInLanguage.Length; i++)
+            {
+                lettersInLanguage[i] = Convert.ToChar( Convert.ToInt32(firstLetter) + code);
+
+            }
+
+
+        }
+
+
+    }
+}
